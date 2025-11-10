ef55dddeff01c2e057ce99f4f4dc544d9a355624
Luahr Rocha <73291538+luahr13@users.noreply.github.com>
2025-11-10 07:51:05 -0300

    Menssagens e validações de campos

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -8,21 +8,28 @@
 <body>
     <a href="{{ route('user.index') }}">Listar</a>
     <h2>Cadastrar Usuário</h2>
+    @if ($errors->any())
+        @foreach ($errors->all() as $error)
+            <p style="color: #f00;">
+                {{ $error}}
+            </p>
+        @endforeach
+    @endif
 
     <form action="{{ route('user.store') }}" method="POST">
         @csrf
         @method('POST')
 
         <label>Nome:</label>
-        <input type="text" name="name" placeholder="Nome completo">
+        <input type="text" name="name" placeholder="Nome completo" value="{{ old('name') }}">
         <br><br>
 
         <label>E-mail:</label>
-        <input type="email" name="email" placeholder="E-mail">
+        <input type="email" name="email" placeholder="E-mail" value="{{ old('email') }}">
         <br><br>
 
         <label>Senha:</label>
-        <input type="password" name="password" placeholder="Senha com no mínimo 6 caracteres">
+        <input type="password" name="password" placeholder="Senha com no mínimo 6 caracteres" value="{{ old('password') }}">
         <br><br>
 
         <button type="submit">Cadastrar</button>
