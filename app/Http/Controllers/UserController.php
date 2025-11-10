4e74f61ec168c11f3e7df586caccc33083e06673
Luahr Rocha <73291538+luahr13@users.noreply.github.com>
2025-11-10 06:59:20 -0300

    Configurações com BD

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+//use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        // Carrega a view 'user.index'
+        return view('user.index');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [UserController::CLass, 'index'])->name('user.index');
+Route::get('/', [UserController::Class, 'index'])->name('user.index');
