1c1a2f5a97aa797251b6d33091ff132cae439da1
Luahr Rocha <73291538+luahr13@users.noreply.github.com>
2025-11-10 07:15:11 -0300

    Tela o formulário de cadastro criado

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -8,5 +8,23 @@
 <body>
     <a href="{{ route('user.index') }}">Listar</a>
     <h2>Cadastrar Usuário</h2>
+
+    <form action="#" method="POST">
+        @csrf
+        @method('POST')
+
+        <label>Nome:</label>
+        <input type="text" name="name" placeholder="Nome completo">
+        <br><br>
+
+        <label>E-mail:</label>
+        <input type="email" name="email" placeholder="E-mail">
+        <br><br>
+
+        <label>Senha:</label>
+        <input type="password" name="password" placeholder="Senha com no mínimo 6 caracteres">
+        <br><br>
+
+        <button type="submit">Cadastrar</button>
 </body>
 </html>
